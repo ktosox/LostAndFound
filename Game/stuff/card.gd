d3144188b298c879be525ed58c64d9512b2fa702
Ktosox <ktoso3@gmail.com>
2023-12-30 23:17:00 +0100

    altered naming of folders and more card admin stuff
    also added UI as Autoload
    head empty

---
+++ Game/stuff/card.gd
@@ -0,0 +1,9 @@
+class_name Card
+
+extends RigidBody2D
+
+
+
+# needs to swap between Kinewmatic Mode for dragging and Charcter for being held in hand
+
+# needs to load the visual parts on creation
---
+++ Game/ui/game_ui.gd
@@ -0,0 +1,8 @@
+extends Control
+
+
+# what do I need in here?
+
+# audio sliders -> but only after I add some god damn audio
+
+# text printing
